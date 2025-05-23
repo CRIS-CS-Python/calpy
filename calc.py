e3ea8229406b1b7848173edb16d480f25f36410e
Teacher Isaac <teacher@eduk8.us>
2025-05-23 16:06:04 +0700

    starter code for calculator functions

---
+++ calc.py
@@ -0,0 +1,10 @@
+'''
+Calculator functions
+'''
+
+def calculate(expr_list):
+    '''
+    Accepts a list of strings containing numbers and operators.
+    Returns the result of calculating the expression as an `int` or `float`.
+    '''
+    return None
---
+++ test_calc.py
@@ -0,0 +1,32 @@
+'''
+Unit tests for calculator functions
+'''
+
+import unittest
+from calc import calculate
+
+class TestCalc(unittest. TestCase):
+
+    def test_add(self):
+        self.assertEqual(calculate(['7', '+', '3']), 10)
+
+    '''
+    def test_subtract(self):
+        pass
+
+    def test_multiply(self):
+        pass
+
+    # create a divide test function
+
+    def test_divide(self):
+        pass
+
+        with self.assertRaises(ValueError):
+            calc.divide(10, 0)
+    '''
+
+# Run the code within the conditional
+
+if __name__ == '__main__':
+    unittest.main()  
