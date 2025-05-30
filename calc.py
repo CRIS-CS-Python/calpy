46542815b66371330866a9b6a08640e72e12e0ed
Jedi <jedi@cris.ac.th>
2025-05-30 15:42:49 +0700

    added plus and minus operator and added unti test

--- calc.py
+++ calc.py
@@ -14,5 +14,15 @@ def calculate(expr_list):
     # needs to be list of 3 items
     if not isinstance(expr_list, list) or len(expr_list) != 3:
         raise CalcError('Expression must be a list of three strings')
+    
+    num1, op, num2 = expr_list # unpacked value into variables
+
+    if op == "+": 
+        return float(num1) + float(num2)
+    else:
+        raise CalcError(f'Invalid operator: {op}')
 
     return None
+
+
+
--- test_calc.py
+++ test_calc.py
@@ -8,10 +8,60 @@ from calc import *
 class TestCalc(unittest. TestCase):
 
     def test_add(self):
+        # Test 2 ints
         self.assertEqual(calculate(['7', '+', '3']), 10)
+
+        # Test 2 floats
+        self.assertEqual(calculate(['0.4', '+', '9.4']), 9.8)
+
+        # Test float int
+        self.assertEqual(calculate(['0.4', '+', '5']), 5.4)
+
+        # Test int float 
+        self.assertEqual(calculate(['5', '+', '5.4']), 10.4)
+
+        # Test negative positive
+        self.assertEqual(calculate(['-5', '+', '5.4']), 0.4)
+
+        # Test positive negative
+        self.assertEqual(calculate(['5', '+', '-5.4']), -0.4)
+
+        # Test float float int result
+        self.assertEqual(calculate(['5.5', '+', '-6.5']), -1)
+
+
+
+        # bad operator
+        with self.assertRaises(CalcError):
+             calculate(['7', 'plus', '3'])
+
+        # bad operands
+        with self.assertRaises(ValueError):
+            calculate(['d', '+', 'a'])
 
         with self.assertRaises(CalcError):
             calculate('not a list')
+
+
+    def test_subtract(self):
+        # Test 2 ints
+        self.assertEqual(calculate(['7', '-', '3']), 4)
+
+        # Test 2 floats
+        self.assertEqual(calculate(['0.4', '-', '9.4']), 9)
+
+        self.assertEqual(calculate(['0.4', '-', '5']), -4.6)
+
+        self.assertEqual(calculate(['5', '-', '5.4']), 0.4)
+
+        # bad operator
+        with self.assertRaises(CalcError):
+             calculate(['7', 'subtraction', '3'])
+
+        # bad operands
+        with self.assertRaises(ValueError):
+            calculate(['d', '-', 'a'])
+
     '''
     def test_subtract(self):
         pass
